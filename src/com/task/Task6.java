2c107f7e29b83411fb8f383e402a5cd837b79e80
Viachalau <tsarenkovslava@yandex.ru>
2021-02-01 22:11:19 +0300

    refactor

--- src/com/task/Task6.java
+++ src/com/task/Task6.java
@@ -4,11 +4,11 @@ public class Task6 {
 
    static public int showHours(int seconds) {
         return (seconds / 3600);
-    }
+   }
 
     static public int showMinutes(int seconds) {
         return (seconds % 3600) / 60;
-    }
+   }
 
     static public int showSeconds(int seconds) {
         return (seconds % 3600) % 60;
